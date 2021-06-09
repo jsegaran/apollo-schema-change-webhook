29c05756eb0ec1b426daccd05053e937365099f5
Joshua Segaran <joshua.segaran@apollographql.com>
2021-06-09 14:03:11 -0700

    Add simple testing for sha256

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-var express = require('express');
+const express = require('express');
 const https = require('https');
+const crypto = require('crypto');
+const assert = require('assert/strict');
+
 var app = express()
 app.use(express.json())
 
 app.post('/', (req, res) => {
   console.log(JSON.stringify(req.body));
   console.log(JSON.stringify(req.headers));
+  assert.equal("sha256=" + crypto.createHmac('sha256', "secret_key").update(JSON.stringify(req.body)).digest('hex'),
+	  req.headers["x-apollo-signature"]);
   const schemaReq = https.get(req.body.schemaURL, resp => {
     let data = '';
     resp.on('data', (chunk) => {
